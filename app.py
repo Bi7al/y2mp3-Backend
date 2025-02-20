319cce5d438551c6ecd1585b88ce2a1b236f8be7
Muhammad Bilal <bilal.muhammad4573@gmail.com>
2025-02-20 21:07:09 +0500

    Added CORS to Flask

--- app.py
+++ app.py
@@ -10,14 +10,14 @@ app = Flask(__name__)
 @app.route('/')
 def default_function():
     try:
-        return "This is the Default Route for Youtube to Mp3 Site"
+        return jsonify({"message":"This is the Default Route for Youtube to Mp3 Site"})
     except Exception as e:
         app.logger.error(f"Error in default_function: {str(e)}")
         raise
 @app.route('/download/<url>')
 def download_function(url):
     try:
-        return f"This is the recieved url : {url}"
+        return jsonify({"recievedUrl":url});
     except Exception as e:
         app.logger.error(f"Error in default_function: {str(e)}")
         raise
